050b8450cc6de6af28b4480aeae50c9a765d719e
Vatsal Manot <vatsal.manot@yahoo.com>
2026-08-13 01:02:54 +0530

    Updated for release 1.2.0

---
+++ Package.swift
@@ -0,0 +1,62 @@
+// swift-tools-version:6.1.0
+
+import PackageDescription
+import Foundation
+
+let package = Package(
+    name: "combine-schedulers",
+    platforms: [
+        .iOS("13.0"),
+        .macOS("10.15"),
+        .tvOS("13.0"),
+        .watchOS("6.0")
+    ],
+    products: [
+        .library(
+            name: "CombineSchedulers",
+            targets: ["CombineSchedulers_PrecompiledProduct"]
+        )
+    ],
+    traits: [
+        .trait(
+            name: "OpenCombineSchedulers",
+            description: "Support for Combine on non-Apple platforms using OpenCombine."
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/swift-precompiled/swift-concurrency-extras",
+            exact: "1.4.1"
+        ),
+        .package(
+            url: "https://github.com/swift-precompiled/xctest-dynamic-overlay",
+            exact: "1.11.0"
+        )
+    ],
+    targets: [
+        .target(
+            name: "CombineSchedulers_Aggregation",
+            dependencies: ["CombineSchedulers"],
+            swiftSettings: [.define("SCIPIO_PRECOMPILED_BINARY_WRAPPER")]
+        ),
+        .binaryTarget(
+            name: "CombineSchedulers",
+            url: "https://github.com/swift-precompiled/combine-schedulers/releases/download/1.2.0/CombineSchedulers-36189eb4df4a468d01b397912a4591f0b7a9a6f5777e53316ba4ba7f1d0c9491.xcframework.zip",
+            checksum: "36189eb4df4a468d01b397912a4591f0b7a9a6f5777e53316ba4ba7f1d0c9491"
+        ),
+        .target(
+            name: "CombineSchedulers_PrecompiledProduct",
+            dependencies: [
+                "CombineSchedulers_Aggregation",
+                .product(
+                    name: "ConcurrencyExtras",
+                    package: "swift-concurrency-extras"
+                ),
+                .product(
+                    name: "IssueReporting",
+                    package: "xctest-dynamic-overlay"
+                )
+            ]
+        )
+    ]
+)
